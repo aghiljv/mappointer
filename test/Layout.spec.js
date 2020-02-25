b32ce49eec329845916bf1448f5035f30f2aba9e
aghiljv <aghiljv@gmail.com>
2020-02-25 20:59:07 +0530

    Layout tests added

--- test/Layout.spec.js
+++ test/Layout.spec.js
@@ -1,10 +1,33 @@
-import { mount } from '@vue/test-utils'
+import { shallowMount } from '@vue/test-utils'
 import Layout from '@/components/Map/Layout'
 
-describe('Layout.vue', ()=> {
-    test('Setup Correctly', ()=> {
-        // window.URL.createObjectURL = function() {};
-        // const wrapper = mount(Layout);
-        expect(true).toBe(true);
-    })
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn()
+  })),
+  NavigationControl: jest.fn()
+}))
+
+describe('Layout.vue', () => {
+
+  test('Passcoords test', () => {
+    const wrapper = shallowMount(Layout)
+    wrapper.vm.passCoords([
+      [45, 34],
+      [78, 23]
+    ])
+    expect(wrapper.vm.coordinates).toEqual([
+      [45, 34],
+      [78, 23]
+    ])
+  })
+
+  test('PassDistance test', () => {
+    const wrapper = shallowMount(Layout)
+    wrapper.vm.passDistance(314.40)
+    expect(wrapper.vm.distance).toBe(314.40)
+  })
 })
--- test/MapHolder.spec.js
+++ test/MapHolder.spec.js
@@ -1,13 +1,21 @@
 import { mount } from '@vue/test-utils'
 import MapHolder from '@/components/Map/MapHolder'
 
+jest.mock('mapbox-gl/dist/mapbox-gl', () => ({
+  GeolocateControl: jest.fn(),
+  Map: jest.fn(() => ({
+    addControl: jest.fn(),
+    on: jest.fn(),
+    remove: jest.fn()
+  })),
+  NavigationControl: jest.fn()
+}))
+
 describe('MapHolder.vue', () => {
-  test('Setup Correctly', () => {
-    // window.URL.createObjectURL = function() {}
-    // const html = document.createElement('div')
-    // html.innerHTML = `<div id="mapContainer"></div>`
-    // document.body.appendChild(html)
-    // const wrapper = mount(MapHolder)
-    expect(true).toBe(true)
+
+  test('Emit test', () => {
+    const wrapper = mount(MapHolder)
+    wrapper.vm.$emit('newDistance', 314.40);
+    expect(wrapper.emitted().newDistance).toBeTruthy()
   })
 })
